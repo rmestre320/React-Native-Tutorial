7544313770ba7b89f7302ba53331bf88732f1a5b
Ronald Mestre <ronaldfelipem@gmail.com>
2024-01-04 10:42:45 -0500

    Create cat const

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Cat = props => {
+  const name = "Bucky"
+  return <Text>Hello, I am your cat {props.name}!</Text>;
+};
+
+const getFullName = (firstName, secondName, thirdName) => {
+  return firstName + ' ' + secondName + ' ' + thirdName;
+};
+
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>App.js has already been opened</Text>
       <StatusBar style="auto" />
+      <Cat name="Bucky"/>
+      <Cat/>
+      <Text>{getFullName('Ron','Don','Bon')}</Text>
     </View>
+    
   );
 }
 
