f41e3768675a32c1a3430b1f775a26f56eaa30d1
Ronald Mestre <ronaldfelipem@gmail.com>
2024-01-05 17:42:20 -0500

    Basic elements

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import { useState, Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button, Alert } from 'react-native';
 
 const Cat = props => {
   const name = "Bucky"
@@ -9,7 +10,46 @@ const Cat = props => {
 const getFullName = (firstName, secondName, thirdName) => {
   return firstName + ' ' + secondName + ' ' + thirdName;
 };
+const BasicButton = props => {
+  //props.title = props.title ? "Press Me" : "a";
+  return <Button
+    onPress={() => {
+      console.log('You tapped the button!');
+    }}
+    title={props.title}
+    
+/>
+};
 
+const AlertButton = props => {
+  return <Button
+    onPress={() => {
+      Alert.alert('Why did you press me??');
+    }}
+    title = {props.title}
+  
+  />
+};
+
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
+  return (
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text
+          .split(' ')
+          .map(word => word && '🍕')
+          .join(' ')}
+      </Text>
+    </View>
+  );
+};
 
 export default function App() {
   return (
@@ -18,8 +58,10 @@ export default function App() {
       <Text>App.js has already been opened</Text>
       <StatusBar style="auto" />
       <Cat name="Bucky"/>
-      <Cat/>
       <Text>{getFullName('Ron','Don','Bon')}</Text>
+      <PizzaTranslator/>
+      <BasicButton title="Press me"/>
+      <AlertButton title="Don't press me"/>
     </View>
     
   );
@@ -32,4 +74,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
